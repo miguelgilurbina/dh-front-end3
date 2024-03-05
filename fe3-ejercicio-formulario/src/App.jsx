533f8f6794c02f42c832cfe337a0d16a530c1711
MrChocolate <mrchocolatepr@gmail.com>
2024-03-05 14:07:56 -0300

    agregando ejercicio de Form + onChange + useState

--- fe3-ejercicio-formulario/src/App.jsx
+++ fe3-ejercicio-formulario/src/App.jsx
@@ -1,34 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const onChangeUserName = (e) => setUserName(e.target.value)
+  const onChangePassword = (e) => setPassword(e.target.value)
+  const onSubmitForm = (e) => {
+    e.preventDefault();
 
+    alert(`Bienvenido ${userName}`)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   
+<div className="App">
+<h3>Iniciar Sesión</h3>
+{/* Pasamos nuestro manejador al evento onSubmit */}
+<form onSubmit={onSubmitForm}>
+{/*
+Creamos dos inputs controlados
+pasando el estado como value y el manejador
+al evento onChange
+*/}
+<input
+type="text"
+placeholder="Nombre de usuario"
+value={userName}
+onChange={onChangeUserName}
+/>
+<input
+type="password"
+placeholder="Password"
+value={password}
+onChange={onChangePassword}
+/>
+{/*
+Mediante el type nos aseguramos que
+se dispare el evento onSubmit al hacer click
+en el botón
+*/}
+<button type="submit">Enviar</button>
+</form>
+</div>
+
+    
   )
 }
 
